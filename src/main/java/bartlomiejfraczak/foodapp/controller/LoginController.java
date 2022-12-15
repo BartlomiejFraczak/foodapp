f74e07206299f4ef3ac46e91d1dd610a1e9024b4
thefr <thefr@192.168.1.175>
2022-12-15 06:39:32 +0100

    test commit

--- src/main/java/bartlomiejfraczak/foodapp/controller/LoginController.java
+++ src/main/java/bartlomiejfraczak/foodapp/controller/LoginController.java
@@ -5,15 +5,10 @@
 package bartlomiejfraczak.foodapp.controller;
 
 import bartlomiejfraczak.foodapp.encje.Uzytkownik;
-import bartlomiejfraczak.foodapp.repo.dao.TekstDao;
 import bartlomiejfraczak.foodapp.repo.dao.UzytkownikDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
